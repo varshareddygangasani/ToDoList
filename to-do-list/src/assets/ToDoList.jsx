259b88c03d85fff2a597331375703ca2f8530634
varshareddygangasani <gangasanivarshareddy@gmail.com>
2025-05-26 12:20:48 +0530

    full todo list with add, delete, clear,moveup,movedown and localStorage persistence

--- to-do-list/src/assets/ToDoList.jsx
+++ to-do-list/src/assets/ToDoList.jsx
@@ -1,26 +1,86 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './ToDoList.css';
 
 function ToDoList() {
-    const [newtask,setnewtask]=useState(['app','boo'])
-    function changeinput(event){
-        setnewtask(event.target.value)
+    const [tasks, settasks] = useState([]);
+    const [newtask, setnewtask] = useState("");
+
+    function changeinput(event) {
+        setnewtask(event.target.value);
     }
-  return (
-    <div>
-        <div>
-            <h1>To-Do-List</h1>
-            <input type='text' value={newtask} placeholder='Enter a task' onChange={changeinput}/>
-            <button>add</button>
-        </div>
-        <div>
-            <ol>
-                {newtask.map((task ,index)=><li key='index'>{task}<button>Delete</button><button>up</button><button>down</button></li>)}
-            </ol>
-            
+
+    function additem() {
+        if (newtask.trim() !== '') {
+            settasks(t => [...t, newtask]);
+            setnewtask("");
+        }
+    }
+    function clearitem(){
+        settasks(t=>[]);
+    }
+
+    function delitem(index) {
+        settasks(tasks.filter((_, i) => i !== index));
+    }
+
+    function moveup(index) {
+        if (index > 0) {
+            const updatedar = [...tasks];
+            [updatedar[index], updatedar[index - 1]] = [updatedar[index - 1], updatedar[index]];
+            settasks(updatedar);
+        }
+    }
+
+    function movedown(index) {
+        if (index < tasks.length - 1) {
+            const updatedar = [...tasks];
+            [updatedar[index], updatedar[index + 1]] = [updatedar[index + 1], updatedar[index]];
+            settasks(updatedar);
+        }
+    }
+
+    return (
+        <div className="todo-container">
+            <h1 className="todo-title">To-Do List</h1>
+            <div className="todo-input-group">
+                <input
+                type="text"
+                value={newtask}
+                placeholder="Enter a task"
+                onChange={changeinput}
+                className="todo-input"
+                />
+                <button onClick={additem} className="todo-add-button">
+                Add
+                </button>
+                <button onClick={clearitem} className="todo-add-button">
+                clear all
+                </button>
+            </div>
+
+            <div className="todo-list">
+                <ol>
+                {tasks.map((task, index) => (
+                    <li key={index} className="todo-item">
+                    <span className="todo-task">{task}</span>
+                    <div className="todo-buttons">
+                        <button className="todo-btn delete" onClick={() => delitem(index)}>
+                        Delete
+                        </button>
+                        <button className="todo-btn up" onClick={() => moveup(index)}>
+                        Up
+                        </button>
+                        <button className="todo-btn down" onClick={() => movedown(index)}>
+                        Down
+                        </button>
+                    </div>
+                    </li>
+                ))}
+                </ol>
+            </div>
         </div>
 
-    </div>
-  )
+    );
 }
 
-export default ToDoList
+export default ToDoList;
